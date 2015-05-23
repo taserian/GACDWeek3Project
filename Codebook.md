b3dfcab2325643301518227f6531dcb50bd7d56f
Antonio Rodriguez <taserian@gmail.com>
2015-05-23 16:02:37 -0400

    First commit

---
+++ Codebook.md
@@ -0,0 +1,124 @@
+---
+title: "Codebook"
+author: "Antonio Rodriguez"
+date: "05/23/2015"
+output: html_document
+---
+
+# Codebook.md
+
+## Summarizing Human Activity Recognition Using Smartphones Dataset 
+
+### Study Design
+
+The original data here was obtained from:
+[https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip]
+
+This data comes from the study linked:
+[http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones#]
+
+From the description of this study:
+
+> The features selected for this database come from the accelerometer and gyroscope 3-axial raw signals tAcc-XYZ and tGyro-XYZ. These time domain signals were captured at a constant rate of 50 Hz. Then they were filtered using a median filter and a 3rd order low pass Butterworth filter with a corner frequency of 20 Hz to remove noise. Similarly, the acceleration signal was then separated into body and gravity acceleration signals BodyAcc-XYZ and GravityAcc-XYZ using another low pass Butterworth filter with a corner frequency of 0.3 Hz. 
+
+> Subsequently, the body linear acceleration and angular velocity were derived in time to obtain Jerk signals (BodyAccJerk-XYZ and BodyGyroJerk-XYZ). Also the magnitude of these three-dimensional signals were calculated using the Euclidean norm (BodyAccMag, GravityAccMag, BodyAccJerkMag, BodyGyroMag, BodyGyroJerkMag). 
+
+> Finally a Fast Fourier Transform (FFT) was applied to some of these signals producing BodyAcc-XYZ, BodyAccJerk-XYZ, BodyGyro-XYZ, BodyAccJerkMag, BodyGyroMag, BodyGyroJerkMag. 
+
+The dataset in this project contains the average of each of the mean and standard deviation measurements in the original study, grouped across each subject and activity.
+
+### Code book
+
+Variables contained in the output data:
+
+**Subject**
+
+The subject ID.
+
+
+**Activity**
+
+The activity the subject was doing. It can be one of the following values:
+
+  - LAYING
+  - SITTING
+  - STANDING
+  - WALKING
+  - WALKING_DOWNSTAIRS
+  - WALKING_UPSTAIRS
+
+
+**Time Domain Signals**
+
+  - TimeBodyAccMeanX
+  - TimeBodyAccMeanY
+  - TimeBodyAccMeanZ
+  - TimeBodyAccStdDevX
+  - TimeBodyAccStdDevY
+  - TimeBodyAccStdDevZ
+  - TimeGravityAccMeanX
+  - TimeGravityAccMeanY
+  - TimeGravityAccMeanZ
+  - TimeGravityAccStdDevX
+  - TimeGravityAccStdDevY
+  - TimeGravityAccStdDevZ
+  - TimeBodyAccJerkMeanX
+  - TimeBodyAccJerkMeanY
+  - TimeBodyAccJerkMeanZ
+  - TimeBodyAccJerkStdDevX
+  - TimeBodyAccJerkStdDevY
+  - TimeBodyAccJerkStdDevZ
+  - TimeBodyGyroMeanX
+  - TimeBodyGyroMeanY
+  - TimeBodyGyroMeanZ
+  - TimeBodyGyroStdDevX
+  - TimeBodyGyroStdDevY
+  - TimeBodyGyroStdDevZ
+  - TimeBodyGyroJerkMeanX
+  - TimeBodyGyroJerkMeanY
+  - TimeBodyGyroJerkMeanZ
+  - TimeBodyGyroJerkStdDevX
+  - TimeBodyGyroJerkStdDevY
+  - TimeBodyGyroJerkStdDevZ
+  - TimeBodyAccMagMean
+  - TimeBodyAccMagStdDev
+  - TimeGravityAccMagMean
+  - TimeGravityAccMagStdDev
+  - TimeBodyAccJerkMagMean
+  - TimeBodyAccJerkMagStdDev
+  - TimeBodyGyroMagMean
+  - TimeBodyGyroMagStdDev
+  - TimeBodyGyroJerkMagMean
+  - TimeBodyGyroJerkMagStdDev
+
+
+**Frequency Domain Signals**
+
+  - FrequencyBodyAccMeanX
+  - FrequencyBodyAccMeanY
+  - FrequencyBodyAccMeanZ
+  - FrequencyBodyAccStdDevX
+  - FrequencyBodyAccStdDevY
+  - FrequencyBodyAccStdDevZ
+  - FrequencyBodyAccJerkMeanX
+  - FrequencyBodyAccJerkMeanY
+  - FrequencyBodyAccJerkMeanZ
+  - FrequencyBodyAccJerkStdDevX
+  - FrequencyBodyAccJerkStdDevY
+  - FrequencyBodyAccJerkStdDevZ
+  - FrequencyBodyGyroMeanX
+  - FrequencyBodyGyroMeanY
+  - FrequencyBodyGyroMeanZ
+  - FrequencyBodyGyroStdDevX
+  - FrequencyBodyGyroStdDevY
+  - FrequencyBodyGyroStdDevZ
+  - FrequencyBodyAccMagMean
+  - FrequencyBodyAccMagStdDev
+  - FrequencyBodyAccJerkMagMean
+  - FrequencyBodyAccJerkMagStdDev
+  - FrequencyBodyGyroMagMean
+  - FrequencyBodyGyroMagStdDev
+  - FrequencyBodyGyroJerkMagMean
+  - FrequencyBodyGyroJerkMagStdDev
+
+
---
+++ run_analysis.R
@@ -0,0 +1,144 @@
+# This script will create a tidy data file following the five steps 
+# detailed in the project.
+
+# If you'd like to reduce the memory footprint while this data is being 
+# processed, set the cleanUp variable to c(TRUE, TRUE, TRUE, TRUE, TRUE).
+# It's currently set to all FALSE so that, when you run it, you can verify 
+# all steps have been completed properly. cleanUp[X] is used at 
+# the end of step X to remove items in memory that aren't used in step X+1. 
+# (My mother raised me to clean up after myself. Happy Mother's Day!)
+cleanUp <- c(FALSE, FALSE, FALSE, FALSE, FALSE)
+
+# 1.- Merge the training and the test sets to create one data set.
+
+# This script assumes that the script is in the same folder as the data 
+# for this project. I.E. The "train" and "test" folders  and the files 
+# "activity_labels.txt", "features.txt", etc. should be in the
+# same folder as this script.
+
+library(plyr); library(dplyr)
+
+# Starting with the training data, let's add labels and subjects.
+
+trainingDataSet <- read.table("train/X_train.txt", sep="")
+
+trainingDataSetLabels <- read.table("train/y_train.txt", sep="")
+names(trainingDataSetLabels)[1] <- "A"
+trainingDataSet <- cbind(trainingDataSetLabels, trainingDataSet)
+
+trainingDataSetSubjects <- read.table("train/subject_train.txt", sep="")
+names(trainingDataSetSubjects)[1] <- "S"
+trainingDataSet <- cbind(trainingDataSetSubjects, trainingDataSet)
+
+# We do the same with the test data, adding labels and subjects
+
+testDataSet <- read.table("test/X_test.txt", sep="")
+
+testDataSetLabels <- read.table("test/y_test.txt", sep="")
+names(testDataSetLabels)[1] <- "A"
+testDataSet <- cbind(testDataSetLabels, testDataSet)
+
+testDataSetSubjects <- read.table("test/subject_test.txt", sep="")
+names(testDataSetSubjects)[1] <- "S"
+testDataSet <- cbind(testDataSetSubjects, testDataSet)
+
+# Now we merge them
+
+combinedDataSet <- rbind(testDataSet, trainingDataSet)
+
+if (cleanUp[1]) {
+  remove(testDataSet, testDataSetLabels, testDataSetSubjects, 
+       trainingDataSet, trainingDataSetLabels, trainingDataSetSubjects)
+}
+
+# 2.- Extract only the measurements on the mean and standard deviation for 
+# each measurement. 
+
+# First, we should add the column names we have for these.
+
+columnNamesFile <- read.table("features.txt", sep="")
+columnNames <- as.character(columnNamesFile[, 2])
+
+# Add the two column names we added to the training and test data sets,
+# and are now in the combined data set.
+columnNames <- c("Subject", "ActivityId", columnNames)
+names(combinedDataSet) <- columnNames
+
+# Now, we keep only the variables (columns) that have either "-mean()", 
+# "-std()", "ActivityId" or "Subject" in their names
+
+extractedColumnNames <- grep("-mean\\(\\)|-std\\(\\)|ActivityId|Subject",
+                             columnNames)
+extractedDataSet <- combinedDataSet[extractedColumnNames]
+
+# Clean up!
+if (cleanUp[2]) {
+    remove(columnNamesFile, columnNames, combinedDataSet, extractedColumnNames)
+}
+
+# 3.- Use descriptive activity names to name the activities in the data set
+
+# Let's make sure the activities are named instead of numbered.
+
+activityNames <- read.table("activity_labels.txt", sep="", 
+                                col.names=c("ActivityId", "Activity")) 
+extractedDataSet <- merge(extractedDataSet, activityNames, by="ActivityId")
+
+activityDataSet <- extractedDataSet[ ,
+                        -which(names(extractedDataSet) %in% c("ActivityId"))]
+
+#Clean up!
+if (cleanUp[3]) {
+    remove(activityNames, extractedDataSet)
+}
+
+# 4.- Appropriately label the data set with descriptive variable names. 
+
+# The current names are kind of ugly, so I'll create a function ...
+
+cleanUpNames <- function(columnName) {
+    # Change "-mean()" to "Mean"
+    columnName <- gsub("-mean\\(\\)", "Mean", columnName) 
+    # Change "-std()" to "StdDev"
+    columnName <- gsub("-std\\(\\)", "StdDev", columnName) 
+    # Remove "-"
+    columnName <- gsub("[-]", "", columnName)  
+    # Columns that start with "f" should instead start with "Frequency"
+    columnName <- gsub("^f", "Frequency", columnName)  
+    # Columns that start with "t" should instead start with "Time"
+    columnName <- gsub("^t", "Time", columnName)  
+    # Change "BodyBody" to "Body"
+    columnName <- gsub("BodyBody", "Body", columnName)
+    
+    columnName
+}
+
+# ... and lapply it to the extractedDataSet
+
+activityDataSetColumns <- names(activityDataSet)
+names(activityDataSet) <- lapply(activityDataSetColumns, cleanUpNames)
+
+# Clean up!
+if (cleanUp[4]) {
+    remove(activityDataSetColumns, cleanUpNames)
+}
+
+# 5.- From the data set in step 4, create a second, independent tidy data set 
+# with the average of each variable for each activity and each subject.
+
+# plyr/dplyr is an immense help here.
+# Apply the function colMeans across all columns that are not "Subject" 
+# and "Activity", subsetted by each pair of Subject * Activity
+
+tidyDataSet <- ddply(activityDataSet, .(Subject, Activity), 
+              .fun = function(x) { colMeans(x[, 
+          -which(names(activityDataSet) %in% c("Subject", "Activity"))])} )
+
+# Clean up!
+if (cleanUp[5]) {
+    remove(activityDataSet)
+}
+
+write.table(tidyDataSet, "TidyData.txt", row.name = FALSE)
+
+
